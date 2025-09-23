5cb99954dc51539b4c62705dec6921a7f05d3af0
zzc <zhangzhichao0601@163.com>
2025-09-24 00:50:03 +0800

    增加抖动，避免纯随机采样可能过于分散和集中

--- src/render/camera.h
+++ src/render/camera.h
@@ -38,11 +38,21 @@ public:
 			for (int i = 0; i < image_width; i++)
 			{
 				color pixel_color(0, 0, 0);
-				for (int sample = 0; sample < samples_per_pixel; sample++)
+
+				for (int s_j = 0; s_j < sqrt_spp; s_j++)
 				{
-					ray r = get_ray(i, j);
-					pixel_color += ray_color(r, max_depth, world);
+					for (int s_i = 0; s_i < sqrt_spp; s_i++)
+					{
+						ray r = get_ray(i, j, s_i, s_j);
+						pixel_color += ray_color(r, max_depth, world);
+					}
 				}
+
+				// for (int sample = 0; sample < samples_per_pixel; sample++)
+				// {
+				// 	ray r = get_ray(i, j);
+				// 	pixel_color += ray_color(r, max_depth, world);
+				// }
 				write_color(out, pixel_samples_scale * pixel_color);
 			}
 		}
@@ -53,6 +63,8 @@ public:
 private:
 	int image_height = 0; // Rendered image height
 	double pixel_samples_scale = 0; // Color scale_ factor for a sum of pixel samples
+	int sqrt_spp; // Square root of number of samples per pixel
+	double recip_sqrt_spp; // 1 / sqrt_spp
 	point3 center; // Camera center
 	point3 pixel00_loc; // Location of pixel 0, 0
 	vec3 pixel_delta_u; // Offset to pixel to the right
@@ -69,6 +81,10 @@ private:
 	{
 		image_height = static_cast<int>(image_width / aspect_ratio);
 		image_height = (image_height < 1) ? 1 : image_height;
+
+		sqrt_spp = static_cast<int>(std::sqrt(samples_per_pixel));
+		pixel_samples_scale = 1.0 / (sqrt_spp * sqrt_spp);
+		recip_sqrt_spp = 1.0 / sqrt_spp;
 
 		pixel_samples_scale = 1.0 / samples_per_pixel;
 
@@ -146,6 +162,37 @@ private:
 
 		auto ray_time = random_double();
 		return ray(ray_origin, ray_direction, ray_time);
+	}
+
+	ray get_ray(int i, int j, int s_i, int s_j) const
+	{
+		// Construct a camera ray originating from the defocus disk and directed at a randomly
+		// sampled point around the pixel location i, j for stratified sample square s_i, s_j.
+
+		auto offset = sample_square_stratified(s_i, s_j);
+		auto pixel_sample = pixel00_loc
+			+ ((i + offset.x()) * pixel_delta_u)
+			+ ((j + offset.y()) * pixel_delta_v);
+
+		auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+		auto ray_direction = pixel_sample - ray_origin;
+		auto ray_time = random_double();
+
+		return ray(ray_origin, ray_direction, ray_time);
+	}
+
+
+	// 抖动算法，把平面分成 sqrt_spp x sqrt_spp 个小格子，在每个小格子内随机采样
+	// 避免了纯随机采样时可能出现的采样点过于集中或过于分散的问题，效果更好
+	vec3 sample_square_stratified(const int s_i, const int s_j) const
+	{
+		// Returns the vector to a random point in the square sub-pixel specified by grid
+		// indices s_i and s_j, for an idealized unit square pixel [-.5,-.5] to [+.5,+.5].
+
+		auto px = ((s_i + random_double()) * recip_sqrt_spp) - 0.5;
+		auto py = ((s_j + random_double()) * recip_sqrt_spp) - 0.5;
+
+		return vec3(px, py, 0);
 	}
 
 	static vec3 sample_square()
